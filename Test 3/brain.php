ac3ef916dfa94e4ad3d4fdaf70bfa4789d32b65c
MDfun <80051098+MDfun@users.noreply.github.com>
2023-01-04 14:19:20 +0100

    Domací úkol
    Test 3 = QuickSort
    Test 4 = SelectionSort

---
+++ Test 3/brain.php
@@ -0,0 +1,19 @@
+<?php
+function bubble_sort($arr) {
+    $n = count($arr);
+    for ($i = 0; $i < $n; $i++) {
+        for ($j = 0; $j < $n - $i - 1; $j++) {
+            if ($arr[$j] > $arr[$j + 1]) {
+                $temp = $arr[$j];
+                $arr[$j] = $arr[$j + 1];
+                $arr[$j + 1] = $temp;
+            }
+        }
+    }
+    return $arr;
+}
+
+$arr = [3,6,4,8,2,1,];
+$sort = bubble_sort($arr);
+echo implode(",",$sort);
+?>
